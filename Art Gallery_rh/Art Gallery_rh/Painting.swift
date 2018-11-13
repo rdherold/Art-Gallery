9858da186a55b65574711963677e4580d0c7adc7
Rob Herold <rderek@bresnan.net>
2018-11-12 20:42:55 -0700

    initial commit

---
+++ Art Gallery_rh/Art Gallery_rh/Painting.swift
@@ -0,0 +1,9 @@
+//
+//  Painting.swift
+//  Art Gallery_rh
+//
+//  Created by Rob Herold on 11/12/18.
+//  Copyright © 2018 Rob Herold. All rights reserved.
+//
+
+import Foundation
---
+++ Art Gallery_rh/Art Gallery_rh/PaintingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Art Gallery_rh
+//
+//  Created by Rob Herold on 11/12/18.
+//  Copyright © 2018 Rob Herold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
